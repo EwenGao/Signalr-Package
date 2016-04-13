cf8c905f38f4b8c5e6e0b03aaf604dfb6991d6db
EwenGao <jayniszhde@126.com>
2016-04-13 11:09:08 +0800

    更新了住要SendMessage函数

--- ChatHub.cs
+++ ChatHub.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Chat.Core
@@ -11,56 +12,38 @@ namespace Chat.Core
         /// 接收客户端推送过来的消息，通过函数名称来判断回调客户端的什么函数
         /// </summary>
         /// <param name="json">json格式的消息数据</param>
-        /// <param name="methodName">函数名称</param>
+        /// <param name="methodName">调用的前端JS函数名称</param>
         /// <param name="groupName">分组字符串</param>
         [HubMethodName("SendMessage")]
         public void SendMessage(string json, string methodName, string groupName)
         {
-            switch (methodName.ToLower())
-            {
-                case "sendmessage":
-                    //把从A客户端接收到的消息推送给同一组的其他客户端
-                    Clients.Group(groupName).ReceiveMessage(json);
-                    break;
-                case "danmu":
-                    //接收客户端的弹幕数据，并推到同一组的客户端
-                    Clients.Group(groupName).DanMu(json);
-                    break;
-                case "privatechat":
-                    //私聊，分组字符串必须在发起私聊前加入到分组中
-                    Clients.Group(groupName).PrivateChat(json);
-                    break;
-            }
+            IClientProxy client = Clients.Group(groupName);
+            client.Invoke(methodName, json);
 
-        }
-        /// <summary>
-        /// 私聊分组添加
-        /// </summary>
-        /// <param name="connectionId">Signalr连接标识</param>
-        /// <param name="groupName">分组字符串</param>
-        public void AddPrivateGroupName(string connectionId, string groupName)
-        {
-            Groups.Add(connectionId, groupName);
         }
         /// <summary>
         /// 推送的消息只有自己可以看
         /// </summary>
         /// <param name="json">json数据</param>
+        [HubMethodName("SendToYourself")]
         public void SendToYourself(string json)
         {
             Clients.Client(Context.ConnectionId).ReceiveYourself(json);
         }
+
         /// <summary>
         /// 接收客户端审核推送的消息
         /// </summary>
         /// <param name="html">html格式的数据</param>
         /// <param name="msgId">被审核消息的编号</param>
+        /// <param name="methodName">调用的前端JS函数名称</param>
         /// <param name="groupName">分组字符串</param>
         [HubMethodName("CheckMessage")]
-        public void CheckMessage(string html, string msgId, string groupName)
+        public void CheckMessage(string html, string msgId,string methodName, string groupName)
         {
             //调用客户端审核通过的js函数
-            Clients.Group(groupName).CheckedMessage(html, msgId);
+            IClientProxy client = Clients.Group(groupName);
+            client.Invoke(methodName, html, msgId);
         }
         /// <summary>
         /// 清除客户端的消息
@@ -73,6 +56,19 @@ namespace Chat.Core
             //调用客户端清除的js函数
             Clients.Group(groupName).DeletedMessage(msgId);
         }
+
+        [HubMethodName("AddGroup")]
+        public void AddGroup(string connectionId, string groupName)
+        {
+            Groups.Add(connectionId, groupName);
+        }
+
+        //[HubMethodName("CheckGroupMessage")]
+        //public void CheckGroupMessage(string html, string msgId, string groupName)
+        //{
+        //    Clients.Group(groupName).CheckedGroupMessage(html, msgId);
+        //}
+
         /// <summary>
         /// 客户端页面加载成功后，主动链接Signalr，分组字符串如果为空字符串，分组无效
         /// </summary>
@@ -80,23 +76,11 @@ namespace Chat.Core
         [HubMethodName("Connect")]
         public void Connect(string groupName)
         {
-            
             //将分组字符串加入Signalr分组
             Groups.Add(Context.ConnectionId, groupName);
             //把connectionId推到客户端，以便后面使用
             Clients.Client(Context.ConnectionId).Connected(Context.ConnectionId);
+            Clients.Group(groupName).Connectting();
         }
-        /// <summary>
-        /// 关闭浏览器或关闭页面，断开Signalr
-        /// </summary>
-        /// <param name="stopCalled"></param>
-        /// <returns></returns>
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            //调用客户端的断开函数
-            Clients.Client(Context.ConnectionId).Disconnected();
-            return base.OnDisconnected(stopCalled);
-        }
-
     }
 }
